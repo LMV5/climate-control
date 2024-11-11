7c614fe53012e8e00d6a4d6a44ff48623c4d1291
Marina <marina.lap08@gmail.com>
2024-11-11 10:29:00 +0100

    add favicon

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body>
         <NavBar />
         <main>{children}</main>
